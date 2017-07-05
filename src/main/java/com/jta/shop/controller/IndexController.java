377da794c717808f2fd36387b618dce56e57cede
Azozello <deonis.panoff@gmail.com>
2017-07-05 22:39:41 +0300

    Created view

---
+++ src/main/java/com/jta/shop/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.jta.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author azozello
+ * @since 05.07.17.
+ */
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/")
+    public String index(){
+        return "index.html";
+    }
+}
